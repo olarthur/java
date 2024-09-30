affdcb1d8506b65899da5f39f46db7c3496a5d28
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-09-30 17:08:09 -0300

    ex3

---
+++ lg-java-beecrowd/Soma-Simples.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        int a, b, soma;
+
+        a = leia.nextInt();
+        b = leia.nextInt();
+        soma = a + b;
+
+        System.out.printf("Soma = %d\n", soma);
+    }
+}
