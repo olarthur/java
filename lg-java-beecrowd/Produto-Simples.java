31f457606fa4cd29b3c062dbd7b3e80f56f55bb2
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-09-30 17:17:48 -0300

    ex4

---
+++ lg-java-beecrowd/Produto-Simples.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int a, b, Prod;
+        a = leia.nextInt();
+        b = leia.nextInt();
+        Prod = a * b;
+        System.out.printf("PROD = %d\n", Prod);
+        
+    }
+}
