dc3d39de55e6e058585512b813542838a9fea4ca
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-09-30 17:23:00 -0300

    ex7

---
+++ lg-java-beecrowd/Diferença.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int a, b, c, d, diferenca;
+        a = leia.nextInt();
+        b = leia.nextInt();
+        c = leia.nextInt();
+        d = leia.nextInt();
+        diferenca = (a*b - c*d);
+        System.out.printf("DIFERENCA = %d\n", diferenca);
+    }
+}
