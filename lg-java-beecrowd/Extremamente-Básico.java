d78bf712b87c33f434c062366dd5c6d1e2e57adf
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-09-30 17:04:35 -0300

    ex2

--- lg-java-beecrowd/Extremamente-Básico.java
+++ lg-java-beecrowd/Extremamente-Básico.java
@@ -1,16 +1,14 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
-        Scanner leia = new Scanner(System.in);
+       Scanner leia = new Scanner(System.in);
 
-        double area, raio;
+       int a, b, x;
+        a = leia.nextInt();
+        b = leia.nextInt();
 
-        raio = leia.nextDouble();
-        area = 3.14159 * Math.pow(raio, 2);
-
-        System.out.printf("A = %.4f\n", area);
+        x = a + b;
+        System.out.printf("X = %d\n", x);
     }
 }
---
+++ lg-java-beecrowd/Área-do-Circulo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        double area, raio;
+
+        raio = leia.nextDouble();
+        area = 3.14159 * Math.pow(raio, 2);
+
+        System.out.printf("A = %.4f\n", area);
+    }
+}
