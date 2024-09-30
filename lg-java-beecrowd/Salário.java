021f9732096d5c6b2e854359e91dd47c6b0c9b9d
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-09-30 17:24:17 -0300

    ex8

---
+++ lg-java-beecrowd/Salário.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int horas, codigo;
+        Double valor_hora, salario;
+        codigo = leia.nextInt();
+        horas = leia.nextInt();
+        valor_hora = leia.nextDouble();
+        salario = horas * valor_hora;
+        System.out.printf("Number = %d\n", codigo);
+        System.out.printf("Salary = U$ %.2f\n", salario);
+    }
+}
