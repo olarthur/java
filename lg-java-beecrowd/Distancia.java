3310e2af68111f6baa106f51e65f25d203cdd34c
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-01 20:07:05 -0300

    ex16

--- lg-java-beecrowd/Distancia.java
+++ lg-java-beecrowd/Distancia.java
@@ -1,14 +1,12 @@
 import java.util.*;
-
+import java.lang.*;
+import java.io.*;
 class Main {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
-        double x1, y1, x2, y2, distancia;
-        x1 = leia.nextDouble();
-        y1 = leia.nextDouble();
-        x2 = leia.nextDouble();
-        y2 = leia.nextDouble();
-        distancia = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-        System.out.printf("%.4f\n", distancia);    
+        int distancia, tempo;
+        distancia = leia.nextInt();
+        tempo = distancia * 2;
+        System.out.printf(tempo + " minutos");
     }
 }
