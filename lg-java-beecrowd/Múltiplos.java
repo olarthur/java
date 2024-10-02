2d29ccfa05d2b254bf6729e82fc45a94da823348
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-02 16:55:31 -0300

    ex29

---
+++ lg-java-beecrowd/Múltiplos.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        int A, B; 
+        
+        A = leia.nextInt();
+        B = leia.nextInt();
+
+        if (A % B == 0 || B % A == 0) {
+            System.out.println("Sao Multiplos");
+        } else {
+            System.out.println("Nao sao Multiplos");
+        }
+    }
+}
