5e7c05b19a9a85706a2113440a8feb8e3184aa01
Arthur Oliveira <arthur08oliveira01@gmail.com>
2024-10-03 19:41:10 -0300

    ex42

---
+++ lg-java-beecrowd/Intervalo2.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        int n, x;
+        int in = 0;
+        int out = 0;
+        
+        n = leia.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            x = leia.nextInt();
+
+            if (x >= 10 && x <= 20) {
+                in++;
+            } else {
+                out++;
+            }
+        }
+        System.out.println(in + " in");
+        System.out.println(out + " out");
+    }
+}
